0e5d8a80870c7fadddb7f8b26faba504fad39f83
Richard Power <richardpwr271@gmail.com>
2025-07-26 15:19:39 -0400

    wrote the code using function along with else and if statements to get the temp converted from fahrenheit to celsius.

---
+++ index.js
@@ -0,0 +1,21 @@
+function convertToCelsius(fahrenheit) {
+  let celsius = ((fahrenheit - 32) * 5) / 9;
+  return celsius;
+}
+
+function describeTemperature(fahrenheit) {
+  let celsius = convertToCelsius(fahrenheit);
+  if (celsius < 0) {
+    console.log(`${celsius} celsius is very cold`);
+  } else if (celsius < 20) {
+    console.log(`${celsius} celsius is cold`);
+  } else if (celsius < 30) {
+    console.log(`${celsius} celsius is warm`);
+  } else if (celsius < 40) {
+    console.log(`${celsius} celsius is hot`);
+  }
+  if (celsius >= 40) {
+    console.log(`${celsius} celsius is very hot`);
+  }
+}
+describeTemperature(100);
